c80f5cf74e2bd41e4beb63d92da6f268a8f914db
xukongwen <oabdao@gmail.com>
2018-09-28 18:45:29 +0800

    add

--- t2.py
+++ t2.py
@@ -1,4 +1,20 @@
 # -- coding: utf-8 --
+# 人生第一个游戏就这么诞生了，然而并不是幻
 import random
-print(random.randint(1,9))
-computer = random.randint(1,9)
+print("让我们来玩个游戏")
+
+def bidaxiao(computer, user):
+    if computer > user:
+        print("完了，你输了！")
+
+    if computer < user:
+        print("厉害，你赢了！")
+
+    if computer == user:
+        print("啊平手了！重新开始")
+        start()
+
+def start():
+    bidaxiao(random.randint(1,9), int(input("输入1-9：")))
+
+start()
---
+++ t3.py
@@ -0,0 +1,7 @@
+# -- coding: utf-8 --
+a = []
+a.append(2)
+a.append("ok")
+
+
+print(a)
