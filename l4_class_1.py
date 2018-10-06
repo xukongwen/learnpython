61c1a3fd554e2952ef144f72dac304d611636ee1
xukongwen <oabdao@gmail.com>
2018-10-06 12:36:00 +0800

    fdg

--- l4_class_1.py
+++ l4_class_1.py
@@ -5,17 +5,26 @@
 import time;
 import random
 
-# 放置各种词组
+# 放置各种名词
 name_list_a = [
-    '时间','无限','人类','同化','物化','无物','庄周','何以',
+    '时间','人类','同化','物化','无物','庄周','何以',
     '情况','类型'
 ]
 
-# 放置各种词组到达"什么的什么"
+# 放置各种形容词到达"什么的什么"
 name_list_b = [
-    '残酷的','标准','没有序列','含糊不清','暗淡','恬淡',
-    '经过','死亡','渐进'
+    '残酷','标准','没有序列','含糊不清','暗淡','恬淡',
+    '经过','死亡','渐进','无限'
 ]
+
+# 耐久度的词语
+durable_list_a =[
+    '锋利','饱满','结实','松垮','不堪一击',
+
+]
+#设置一个空随机箱子
+random_lootbox = []
+
 
 #随机造名字
 def randname():
@@ -31,7 +40,7 @@ def randname():
     return new_random_name
 
 
-randname()
+#randname()
 #p=randname()
 #print(p)
 
@@ -59,13 +68,17 @@ class Item():
         self.durable = durable
 
     def display_iteminfo(self):
-        print(self.name+":价格:"+str(self.price)+"重量:"+str(self.weight)+'稀有度:'+self.rare+'耐久:'+str(self.durable))
+        output_info1 = self.name+":价格:"+str(self.price)+"重量:"+str(self.weight)+'稀有度:'+self.rare+'耐久:'+str(self.durable)
+        return output_info1
 
     def display_name(self):
         self.name = randname()
         self.price = random.randint(0,101)
         self.weight = random.randint(0,51)
-        print(self.name+':'+'价格:'+str(self.price)+" "+"重量:"+str(self.weight))
+        output_info = self.name+':'+'价格:'+str(self.price)+" "+"重量:"+str(self.weight)
+        #print(self.name+':'+'价格:'+str(self.price)+" "+"重量:"+str(self.weight))
+        return output_info
+
 
 #显示日期和时间
 def showtime():
@@ -91,11 +104,27 @@ def pigdo():
     elif pigact == 2:
         mypig.eat()
 
+#开宝箱完成!
+def lootbox():
+    for i in range(random.randint(0,4)):
+        inlist = str(my_sowrd.display_name())
+        random_lootbox.append(inlist)
 
-my_sowrd = Item('大剑',250,100,'破损',20)
-my_sowrd.display_iteminfo()
-my_sowrd.display_name()
+    print('你打开了一个宝箱')
+    print('.......................')
+    for i in random_lootbox:
+        print(i)
+
+
 showtime()
+my_sowrd = Item('大剑',250,100,'破损',20)
+#my_sowrd.display_iteminfo()
+
 pigdo()
+lootbox()
+
+
+
+
 
 
