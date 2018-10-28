f6f7b70e00963436ea75c363b845039edbcc098d
xukongwen <oabdao@gmail.com>
2018-10-28 09:19:04 +0800

    各种拖拽和窗口的雏形

--- game/game1/game-test1.py
+++ game/game1/game-test1.py
@@ -5,6 +5,17 @@ import phantom_core_1 as pc
 import sys
 import json
 import pickle
+import csv
+import time
+import random
+
+with open('lists/list2.csv','r') as f:
+    reader = csv.reader(f)
+    my_list = list(reader)
+    final_list = my_list[1:]#把第一行切掉
+
+    player_inventory = []
+    print(final_list)
 
 #---------------根据上帝的意志和概念创意,创建一切实例(子民),一个游戏或一个app就是实例的舞台,下面是孵化池-----------------------------------------------------
 
@@ -14,60 +25,151 @@ screen2 = pc.display_init(1400,1000)
 #建立按钮实例
 button1 = pc.Button((5,5),(100,50),screen2.Green,screen2.RED,"EXIT")
 button2 = pc.Button((150,5),(100,50),screen2.Green,screen2.RED,"SAVE")
+b_draw = pc.Button((650,400),(150,50),screen2.Green,screen2.RED,"draw_test")
+b_play = pc.Button((650,300),(150,50),screen2.Green,screen2.RED,"play_test")
+b_test = pc.Button((650,500),(150,50),screen2.Green,screen2.RED,"test1")
 
 #建立格子实例
 grid2 = pc.Grid(9,50,300,300,2,screen2.Green,screen2.screen)
-#grid1 = pc.Grid(1,30,900,300,1,screen2.RED,screen2.screen)
+
+box1 = pc.Box(300,300,100,100,screen2.Green)
+window1 = pc.Window(700,300,500,400,True,screen2.BLUE,'今者吾丧我',screen2.WHITE)
+
+player1 = pc.Player()
 
 
 grid2.load_grid()
+pp.display.set_caption("Phantom OS")
 
-#-------------------------游戏循环主体,用各种event,让各种子民交互起来,这里是世界舞台--------------------------------------------
+#-------------------------游戏循环主体,用各种event,让各种子民交互起来,这里是世界舞台,舞台可以有好几个--------------------------------------------
 
-
-is_running = True
-#这里是游戏循环事件区域
-while is_running:
-    for event in pp.event.get():#倾听所有输入事件(鼠标,键盘,手柄)
-            #关闭窗口和esc事件
-            if event.type == pp.QUIT:
-                is_running = False
-            elif event.type == pp.KEYDOWN:
-                if event.key == pp.K_ESCAPE:
+def play():
+    is_running = True
+    while is_running:
+        for event in pp.event.get():
+                if event.type == pp.QUIT:
                     is_running = False
+                elif event.type == pp.KEYDOWN:
+                    if event.key == pp.K_ESCAPE:
+                        is_running = False
 
-            #格子系统的格子拖拽事件
-            #grid1.event_check(event)
-            grid2.event_check(event)
+                player1.event_click(event)
 
-            #所有按钮事件
-            if button1.is_clicked(event):#退出游戏
-                is_running = False
-            if button2.is_clicked(event):#储存游戏
-                grid2.save_grid()
-                print('saved!')
-             
+
+        screen2.screen.fill(screen2.WHITE)
+        player1.update()
+
+
+        player1.draw(screen2.screen)
+
+
+        pp.display.update()
+        screen2.clock.tick(60)
+
+
+def test1():
+    is_running = True
+    while is_running:
+        for event in pp.event.get():
+                if event.type == pp.QUIT:
+                    is_running = False
+                elif event.type == pp.KEYDOWN:
+                    if event.key == pp.K_ESCAPE:
+                        is_running = False
+
+                box1.handle_event(event)
+                window1.handle_event(event)
+        screen2.screen.fill(screen2.WHITE)
+
+        window1.draw(screen2.screen)
+        box1.draw(screen2.screen)
         
 
-    #---------------这里是游戏渲染区域-------------------
-    #填充白色背景
-    screen2.screen.fill(screen2.WHITE)
-    #画出按钮
-    button1.update()
-    button2.update()
-    button1.draw(screen2.screen)
-    button2.draw(screen2.screen)
-    #画出格子
-    #grid1.draw_grid()
-    grid2.draw_grid()
-    pp.draw.line(screen2.screen, screen2.BLACK, [700,50],[700,950],1)
-    
-    
-     #更新整体画面  
-    pp.display.update()
-    screen2.clock.tick(60)
+
+
+        pp.display.update()
+        screen2.clock.tick(60)
 
 
 
+def draw():
+    is_running = True
+    while is_running:
+        for event in pp.event.get():#倾听所有输入事件(鼠标,键盘,手柄)
+                #关闭窗口和esc事件
+                if event.type == pp.QUIT:
+                    is_running = False
+                elif event.type == pp.KEYDOWN:
+                    if event.key == pp.K_ESCAPE:
+                        is_running = False
 
+                #格子系统的格子拖拽事件
+            
+                grid2.event_check(event)
+
+                #所有按钮事件
+                if button1.is_clicked(event):#退出游戏
+                    is_running = False
+                if button2.is_clicked(event):#储存游戏
+                    grid2.save_grid()
+                    print('saved!')
+                
+            
+
+        #---------------这里是游戏渲染区域-------------------
+        #填充白色背景
+        screen2.screen.fill(screen2.WHITE)
+        #画出按钮
+        button1.update()
+        button2.update()
+        button1.draw(screen2.screen)
+        button2.draw(screen2.screen)
+        #画出格子
+        #grid1.draw_grid()
+        grid2.draw_grid()
+        pp.draw.line(screen2.screen, screen2.BLACK, [700,50],[700,950],1)
+        
+        
+        #更新整体画面  
+        pp.display.update()
+        screen2.clock.tick(60)
+
+
+#菜单主体
+def menu1():
+    is_running = True
+    while is_running:
+        for event in pp.event.get():
+               
+                if event.type == pp.QUIT:
+                    is_running = False
+                elif event.type == pp.KEYDOWN:
+                    if event.key == pp.K_ESCAPE:
+                        is_running = False
+                
+                if b_draw.is_clicked(event):
+                    draw()
+
+                if b_play.is_clicked(event):
+                    play()
+
+                if b_test.is_clicked(event):
+                    test1()
+
+        screen2.screen.fill(screen2.WHITE)
+
+        b_draw.update()
+        b_play.update()
+        b_test.update()
+
+        b_play.draw(screen2.screen)
+        b_draw.draw(screen2.screen)
+        b_test.draw(screen2.screen)
+
+        pp.display.update()
+        screen2.clock.tick(60)
+
+
+
+menu1()
 pp.quit()
--- game/game1/phantom_core_1.py
+++ game/game1/phantom_core_1.py
@@ -3,6 +3,7 @@
 import pygame as pp
 import sys
 import pickle
+import time
 
 #-------------------------------------------类就是上帝创世区域,全部是抽象设计,没有实体--------------------------------------------------------------------------
 
@@ -47,6 +48,7 @@ class Grid():
         for x in range(self.num):
             for y in range (self.num):
                 self.rects.append(pp.Rect(x*(self.b_size+self.g_space)+self.g_x, y*(self.b_size+self.g_space)+self.g_y, self.b_size, self.b_size ))
+                #time.sleep(10)
         self.selected = None
 
     #检查格子触发事件,和激活按下格子事件
@@ -89,14 +91,14 @@ class Grid():
             self.flag = True
         else:
             for b in self.rects:
-                if b.x > 700 and b.y < 500:
-                    #self.b_color = (255,0,0)
-                    #print(b)
+                if b.x > 700 and b.y < 500:#这里放了临时的测试各自交互的，根据位置变色
+            
                     pp.draw.rect(self.sc, (255,0,0),b)
                 elif b.y > 500 and b.x > 700:
                     pp.draw.rect(self.sc, ( 0,  0, 255),b)
                 else: 
                     pp.draw.rect(self.sc, self.b_color, b)
+                    
 
     #读档功能
     def load_grid(self):
@@ -114,8 +116,86 @@ class Grid():
        
         pickle_out.close()
 
+#需要一个移动（拖动）的父类
+#需要一个格子的夫类
 
+
+class Moveable():
+    def handle_event(self, event):
+        #global redrawing
+
+        if event.type == pp.MOUSEBUTTONDOWN:
+            if self.hover:
+                if event.button == 1:
+                    if self.movable:
+                        self.moving = True
+                       # redrawing = True
+                        return True
+                elif event.button == 3:
+                    print(self.rect.topleft)
+                    return True
+                    
+        if event.type == pp.MOUSEBUTTONUP:
+            
+            if self.hover:
+                if self.movable and self.moving:
+                    self.moving = False
+                    #redrawing = True
+                    return True
+
+        if event.type == pp.MOUSEMOTION:
+            self.hover = self.rect.collidepoint(event.pos)
+            
+            if self.movable and self.moving:
+                self.rect.move_ip(event.rel)
+                #print(event.rel[0])
+                #self.x = self.x + event.rel[0]
+                #redrawing = True
+                return True
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+
+class Box(Moveable):
+    def __init__(self,x,y,w,h,color=(0, 100, 255)):
+        self.hover = True
+        self.color = color
+        self.rect = pp.Rect(x, y, w, h)
+        self.image = pp.Surface((w,h))
+        self.image.fill(self.color)
         
+        self.moving = False
+        self.movable = True
+
+        pp.draw.rect(self.image, self.color, self.rect)
+
+
+class Window(Moveable):
+    def __init__(self, x, y, width, height, movable=True, color=(0,0,0), text='ok', text_color=(0,0,0)):
+        self.hover = True
+        self.moving = False
+        self.movable = movable
+        self.color = color
+        self.text = text 
+        self.text_color = text_color
+
+        self.rect = pp.Rect(x, y, width, height)
+        self.image = pp.Surface((width,height))
+        self.image.fill(self.color) 
+        
+        self.x = x
+        self.y = y
+        self.font = pp.font.Font('华文宋体.ttf', 26)
+        self.width =width
+        self.height = height
+
+        pp.draw.rect(self.image, self.color, self.rect) 
+        text_image = self.font.render(self.text, True, self.text_color)
+        text_rect = text_image.get_rect()
+        self.image.blit(text_image, (10, 5, *text_rect.size))
+    
+
 
 
 #按钮的本尊
@@ -131,7 +211,7 @@ class Button(object):
         self.image_hovered = pp.Surface(size)
         self.image_hovered.fill(h_color)
 
-        font = pp.font.Font(None,32)
+        font = pp.font.Font(None,24)
         text = font.render(text, True,(0,0,0))
         text_rect = text.get_rect()
         text_rect.center = self.rect.center
@@ -159,12 +239,129 @@ class Button(object):
             self.hovered = self.rect.collidepoint(event.pos)
         elif event.type == pp.MOUSEBUTTONDOWN:
             if self.hovered:
-            
-            #if event.button == 1:
-                
                 return self.rect.collidepoint(event.pos)#事件
                 
+#玩家的本尊
+class Player():
+    def __init__(self):
+        self.image = pp.Surface((50,100))
+        self.image.fill((   0, 255,   0))
+        self.rect = self.image.get_rect()
+        self.rect.x = 700
+        self.rect.y = 500
+        #下面设这些开关很好用，所有的update在一个地方，其他按键只是激活开关
+        self.m_up = False
+        self.m_down = False
+        self.m_left = False
+        self.m_right = False
+      
 
+#接收键盘信息
+    def event_click(self,event):
+            if event.type == pp.KEYDOWN:
+                if event.key == pp.K_UP:
+                    self.m_up = True
+                if event.key == pp.K_DOWN:
+                    self.m_down = True
+                if event.key == pp.K_RIGHT:
+                    self.m_right = True
+                if event.key == pp.K_LEFT:
+                    self.m_left = True
+            
+            elif event.type == pp.KEYUP:
+                if event.key == pp.K_UP:
+                    self.m_up = False
+                if event.key == pp.K_DOWN:
+                    self.m_down = False
+                if event.key == pp.K_RIGHT:
+                    self.m_right = False
+                if event.key == pp.K_LEFT:
+                    self.m_left = False
+
+#接受到开关信息后更新player的位置           
+    def update(self):
+        if self.m_up == True:
+            self.rect.y -= 10
+            if self.rect.y < 0:
+                self.rect.y = 1000
+
+        if self.m_down == True:
+            self.rect.y += 10
+            if self.rect.y > 1000:
+                self.rect.y = 0
+            
+        if self.m_left == True:
+            self.rect.x -= 10
+            if self.rect.x < 0:
+                self.rect.x = 1400
+
+        if self.m_right == True:
+            self.rect.x += 10
+            if self.rect.x > 1400:
+                self.rect.x = 0
+      
+
+
+
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+
+#物品类
+class Item():
+    dur=0
+    def __init__(self,name,item_type,wuxing,n_name,num,weight,price,att,dur,rare,player_dis):
+        self.name = name
+        self.type = item_type
+        self.wuxing = wuxing
+        self.n_name = n_name
+        self.num = num
+        self.weight = weight
+        self.price = price
+        self.att = att
+        self.dur = dur  
+        self.rare = rare
+        self.player_dis = player_dis
+
+    def info(self):#存成一个自己的list
+        item_info_list = [self.name,self.type,self.wuxing,self.n_name,self.num,self.weight,self.price,self.att,self.dur,self.rare,self.player_dis]
+        return item_info_list
+
+    def fixdur(self):#临时的修理
+        self.dur = int(self.dur) + 10
+        return self.dur
+
+    def print_info(self):#临时的打印所有信息
+        print_info = self.name,self.type,self.wuxing,self.n_name,self.num,self.weight,self.price,self.att,self.dur,self.rare,self.player_dis
+        return print_info
+
+
+#写了一个箱子的类,用来事先存放东西,然后可以拿走,和放入
+class Loot_box():
+    def __init__(self,item_list,item_num):
+        self.item_list = item_list#这里输入是一个列表,就可以同时输入n个物品!
+        self.item_num = item_num
+        self.loot_box = []#本箱子的最终list
+        self.make_item()
+        
+
+    def make_item(self):
+        for i in self.item_list:#根据输入的list,把物品装入
+            temp_item = Item(*final_list[int(i)])#类里面可以调用其他的类,这里是展开list列表,填入属性
+            self.loot_box.append(temp_item.info())#加入根据编号的物品进入箱子
+
+    def take_item(self):#拿走东西,并添加给玩家,一定要先给再删除
+        player_inventory.append(self.loot_box[2])
+        del self.loot_box[2]
+
+    def add_item(self):#玩家向箱子添加
+        self.loot_box.append(player_inventory[0])
+        del player_inventory[0]
+
+    def show_boxname(self):
+        boxname = 'box'
+        return boxname
 
 
 
